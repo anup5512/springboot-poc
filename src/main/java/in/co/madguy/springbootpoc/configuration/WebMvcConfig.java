f2b7c2cf68b9db4ca5d0bc3a6fc4a5bc0b2793f8
Anup Anand <anup5512@gmail.com>
2019-03-05 23:38:18 +0530

    Updated config

--- src/main/java/in/co/madguy/springbootpoc/configuration/WebMvcConfig.java
+++ src/main/java/in/co/madguy/springbootpoc/configuration/WebMvcConfig.java
@@ -1,18 +1,14 @@
 package in.co.madguy.springbootpoc.configuration;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.json.Json;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -48,15 +44,28 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
             .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+    /**
+     * Method to register HTTP Message Converters
+     * Note: Gson can be alternatively registered from application.properties
+     * Refer:
+     * spring.http.converters.preferred-json-mapper=gson
+     * spring.gson.date-format=YYYY-MM-DD'T'HH:mm:ss
+     *
+     * @see <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/common-application-properties.html">Spring application properties</a>
+     * @see <a href="https://www.callicoder.com/configuring-spring-boot-to-use-gson-instead-of-jackson/">Blog</a>
+     *
+     * @param converters
+     */
     @Override
     protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        Gson gson = new GsonBuilder()
+        // Using Java Config: Register custom HTTP Message Converters
+        /*Gson gson = new GsonBuilder()
             .setDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'")
             .registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter())
             .create();
         GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
         gsonHttpMessageConverter.setGson(gson);
         converters.add(gsonHttpMessageConverter);
-        super.configureMessageConverters(converters);
+        super.configureMessageConverters(converters);*/
     }
 }
